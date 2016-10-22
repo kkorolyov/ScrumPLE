b4546fd17f2bd478119b2e59f464d997c82bca56
Ryan Truong <ryantruong1993@gmail.com>
2016-10-22 10:44:13 +0900

    Updated Tables/Constraints
    Foreign Keys are now not null.  Added more tables for project db.

--- src/ScrumPLEDB/Database.java
+++ src/ScrumPLEDB/Database.java
@@ -61,7 +61,12 @@ public class Database {
 			Statement s = conn.createStatement();
 			s.execute("CREATE TABLE IF NOT EXISTS Project " + "(name VARCHAR(64), description VARCHAR(256), PRIMARY KEY (name))");
 			s.execute("CREATE TABLE IF NOT EXISTS Roles (id INT UNSIGNED AUTO_INCREMENT, name VARCHAR(64) NOT NULL , PRIMARY KEY (id))");
-			s.execute("CREATE TABLE IF NOT EXISTS Users (id INT UNSIGNED AUTO_INCREMENT, credentials VARCHAR(128) NOT NULL, name VARCHAR(64) NOT NULL, role INT UNSIGNED, PRIMARY KEY (id),FOREIGN KEY (role) REFERENCES Roles (id))");
+			s.execute("CREATE TABLE IF NOT EXISTS Users (id INT UNSIGNED AUTO_INCREMENT, credentials VARCHAR(128) NOT NULL, name VARCHAR(64) NOT NULL, role INT UNSIGNED NOT NULL, PRIMARY KEY (id),FOREIGN KEY (role) REFERENCES Roles (id))");
+			s.execute("CREATE TABLE IF NOT EXISTS Labels (id INT UNSIGNED AUTO_INCREMENT, name VARCHAR(64) NOT NULL, PRIMARY KEY (id))");
+			s.execute("CREATE TABLE IF NOT EXISTS Tasks (id INT UNSIGNED AUTO_INCREMENT, label INT UNSIGNED NOT NULL, description VARCHAR(256) NOT NULL, hours_left TINYINT NOT NULL, `release` INT, sprint INT, PRIMARY KEY (id), FOREIGN KEY (label) REFERENCES Labels (id))");
+			
+			
+			
 			s.close();
 		}
 		catch (SQLException e) {
