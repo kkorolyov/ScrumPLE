fb8a3f19dc07168a1dcdb50811c2d3b6a273450a
Ryan Truong <ryantruong1993@gmail.com>
2016-11-01 11:53:50 +0900

    Added start/end date to Sprint, and time left.

--- src/org/scrumple/scrumplecore/applications/Sprint.java
+++ src/org/scrumple/scrumplecore/applications/Sprint.java
@@ -1,20 +1,38 @@
 package org.scrumple.scrumplecore.applications;
 
 import java.util.*;
+import java.time.*;
 
 
 
 public class Sprint {
 	private Set<Task> tasks;
 	private Backlog backlog;
+	private Period timeLeft;
+	private LocalDate start;
+	private LocalDate end;
 	
-	public Sprint (Backlog sprintBacklog) {
+	public Sprint (Backlog sprintBacklog, int year, int month, int dayOfMonth) {
 		this.backlog = sprintBacklog;
 		tasks = new HashSet<Task>();
+		start = LocalDate.now();
+		end = LocalDate.of(year, month, dayOfMonth);
 	}
 	
 	public void setTasks() {
 		tasks = backlog.getTasks();
+	}
+	
+	public void setStateDate(int year, int month, int date) {
+		start = LocalDate.of(year, month, date);
+	}
+	
+	public void setEndDate(int year, int month, int date) {
+		end = LocalDate.of(year, month, date);
+	}
+	
+	public void timeLeft() {
+		timeLeft = Period.between(start, end);
 	}
 
 }
