75e5235db3c99fccdcde39c370a7a5d231f7056b
Ryan Truong <ryantruong1993@gmail.com>
2016-11-03 04:43:59 +0900

    Sprint Implements Saveable .

--- src/org/scrumple/scrumplecore/applications/Sprint.java
+++ src/org/scrumple/scrumplecore/applications/Sprint.java
@@ -1,52 +1,79 @@
 package org.scrumple.scrumplecore.applications;
 
 import java.util.*;
+
+import org.scrumple.scrumplecore.database.Saveable;
+
 import java.time.*;
 import java.sql.*
 ;
 import java.sql.Date;
 
-public class Sprint {
+public class Sprint implements Saveable {
+	private String description;
 	private Set<Task> tasks;
 	private Backlog backlog;
 	private Period timeLeft;
 	private LocalDate start, end, current;
-	private Date convertedStartDate, convertedEndDate;
+	//private Date convertedStartDate, convertedEndDate;
 	
-	public Sprint (Backlog sprintBacklog, int year, int month, int dayOfMonth) {
+	public Sprint (String des, Backlog sprintBacklog, int year, int month, int dayOfMonth) {
+		this.description = des;
 		this.backlog = sprintBacklog;
 		tasks = new HashSet<Task>();
 		start = LocalDate.now();
 		end = LocalDate.of(year, month, dayOfMonth);
-		convertedStartDate = Date.valueOf(start);
-		convertedEndDate = Date.valueOf(end);
+		//Not sure if I need these anymore.
+		//convertedStartDate = Date.valueOf(start);
+		//convertedEndDate = Date.valueOf(end);
+	}
+	
+	public String getDes() {
+		return description;
 	}
 	
 	public void setTasks() {
 		tasks = backlog.getTasks();
 	}
 	
-	public void setStateDate(Date d) {
+	public void setStartDate(Date d) {
 		start = d.toLocalDate();
-		convertedStartDate = d;
+		//convertedStartDate = d;
 	}
 	
 	public void setEndDate(Date d) {
 		end = d.toLocalDate();
-		convertedEndDate = d;
+		//convertedEndDate = d;
 	}
 	
-	public Date getConvertedStartDate() {
+	/*public Date getConvertedStartDate() {
 		return this.convertedStartDate;
 	}
 	
 	public Date getConvertedEndDate() {
 		return this.convertedEndDate;
-	}
+	}*/
 	
 	public void timeLeft() {
 		current = LocalDate.now();
 		timeLeft = Period.between(current, end);
+	}
+
+	@Override
+	public List<Object> toData() {
+		// TODO Auto-generated method stub
+		return Arrays.asList(new Object[]{description, Date.valueOf(start), Date.valueOf(end)});
+
+	}
+
+	@Override
+	public void fromData(List<Object> data) {
+		Iterator <Object> it = data.iterator();
+		
+		this.description = (String) it.next();
+		start = ((Date) it.next()).toLocalDate();
+		end = ((Date) it.next()).toLocalDate();
+		
 	}
 
 }
