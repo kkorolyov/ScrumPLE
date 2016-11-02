264422eafed16b0e643dd6c84c3411e11fba5ce6
IamKenshin <ryantruong1993@gmail.com>
2016-11-01 18:57:23 -0700

    Added converted dates to SPrint for easy SQL storing.

--- src/org/scrumple/scrumplecore/applications/Sprint.java
+++ src/org/scrumple/scrumplecore/applications/Sprint.java
@@ -2,20 +2,26 @@ package org.scrumple.scrumplecore.applications;
 
 import java.util.*;
 import java.time.*;
-
-
+import java.sql.*
+;
+import java.sql.Date;
 
 public class Sprint {
 	private Set<Task> tasks;
 	private Backlog backlog;
 	private Period timeLeft;
 	private LocalDate start, end, current;
+	private Date convertedStartDate, convertedEndDate;
 	
+	@SuppressWarnings("deprecation")
 	public Sprint (Backlog sprintBacklog, int year, int month, int dayOfMonth) {
 		this.backlog = sprintBacklog;
 		tasks = new HashSet<Task>();
 		start = LocalDate.now();
 		end = LocalDate.of(year, month, dayOfMonth);
+		String a = "sfdsafa";
+		convertedStartDate = new Date(start.getYear(), start.getDayOfMonth()-1, start.getDayOfMonth());
+		convertedEndDate = new Date(end.getYear(), end.getDayOfMonth()-1, end.getDayOfMonth());
 	}
 	
 	public void setTasks() {
