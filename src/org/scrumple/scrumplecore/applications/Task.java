c28a6381ca8a89e96bc73e570960b6b661effb4f
IamKenshin <ryantruong1993@gmail.com>
2016-11-04 17:53:05 -0700

    Changed taskType to an int to match database. Task implements toData and fromData.

--- src/org/scrumple/scrumplecore/applications/Task.java
+++ src/org/scrumple/scrumplecore/applications/Task.java
@@ -1,12 +1,18 @@
 package org.scrumple.scrumplecore.applications;
 
-public class Task {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.scrumple.scrumplecore.database.Saveable;
+
+public class Task implements Saveable{
 	private String taskName;
-	private String taskType;  //Is it a feature, bug, etc.  Do we need a Label class, or can we just include it as a String field in Task? Current save method will require that we have a Label class.
+	private int taskType;  //Is it a feature, bug, etc.  Do we need a Label class, or can we just include it as a String field in Task? Current save method will require that we have a Label class.
 	private String taskDescription;
 	private boolean done;
 	
-	public Task(String name, String type, String taskDescription) {
+	public Task(String name, int type, String taskDescription) {
 		this.taskName = name;
 		this.taskType = type;
 		this.taskDescription = taskDescription;
@@ -17,7 +23,7 @@ public class Task {
 		return this.taskName;
 	}
 	
-	public String getTaskType() {
+	public int getTaskType() {
 		return this.taskType;
 	}
 	
@@ -30,5 +36,19 @@ public class Task {
 	}
 	public boolean isFinished() {
 		return done;
+	}
+
+	@Override
+	public List<Object> toData() {
+		// TODO Auto-generated method stub
+		return Arrays.asList(new Object[]{taskName, taskType, taskDescription});
+	}
+
+	@Override
+	public void fromData(List<Object> data) {
+		Iterator <Object> it = data.iterator();
+		this.taskName = (String) it.next();
+		this.taskType = (int) it.next();
+		this.taskDescription = (String) it.next();
 	}
 }
