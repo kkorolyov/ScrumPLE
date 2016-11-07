d1647c63dd747828ee53541f6fe5c5f0ba57187d
Ryan Truong <ryantruong1993@gmail.com>
2016-11-07 15:30:54 +0900

    Updated ChatClient

--- src/org/scrumple/scrumplecore/chat/chatClient.java
+++ src/org/scrumple/scrumplecore/chat/chatClient.java
@@ -10,20 +10,51 @@ import dev.kkorolyov.simplelogs.Logger.Level;
 
 import java.io.*;
 
-public class chatClient {
+public class ChatClient {
 	private Socket socket;
 	private ObjectInputStream reader;
 	private ObjectOutputStream writer;
+	private String serverAddress, username;
+	private int port;
 	private static final Logger log = Logger.getLogger(Database.class.getName(), Level.DEBUG, new PrintWriter(System.err));
 
-	public chatClient(){
+	public ChatClient(String address, int port, String username){
+		
+		this.serverAddress = address;
+		this.port = port;
+		this.username = username;
 		socket = new Socket();
+		
+	}
+	
+	public boolean startClient() {
+		
+		
+		try {
+			socket = new Socket(serverAddress, port);
+		} 
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.exception(e);
+			return false;
+		}
+		
 		try {
 			reader = new ObjectInputStream(socket.getInputStream());
 			writer = new ObjectOutputStream(socket.getOutputStream());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			log.exception(e);
+			return false;
 		}
+		
+		try {
+			writer.writeObject(username);
+		} catch (IOException e) {
+			System.out.println("Exception during login by : " + username);
+			log.exception(e);
+			return false;
+		}
+		return true;
 	}
 }
