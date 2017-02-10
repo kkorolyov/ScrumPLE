8d30d51de3ad5cd42c5ce338ba189429ebd049d3
root <root@ip-172-31-35-7.us-west-2.compute.internal>
2017-02-10 15:42:14 -0500

    chat now uses https

--- ScrumPLE-Pretty/content/chat/chat.js
+++ ScrumPLE-Pretty/content/chat/chat.js
@@ -1,6 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var io = require('socket.io')(https);
+var fs = require('fs');
+var https = require('https');
+var privateKey = fs.readFileSync('/etc/ssl/private/node-self.key');
+var certificate = fs.readFileSync('/etc/ssl/certs/node-self.crt');
+var credentials = {key: privateKey, cert: certificate};
+
+//var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+var io = require('socket.io')(httpsServer);
 
 app.get('/', function(req, res){
     res.sendFile('/opt/git/scrumple/ScrumPLE-Pretty/content/chat/chat.html');
@@ -17,6 +26,6 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000, function(){
+httpsServer.listen(3000, function(){
     console.log('listening on *:3000');
 });
