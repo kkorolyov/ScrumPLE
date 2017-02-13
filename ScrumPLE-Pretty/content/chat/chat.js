4a8b3d0151174e059cad120da2638cb666f15fe3
Ryan Truong <ryantruong1993@gmail.com>
2017-02-14 08:48:42 +0900

    Update chat subsystem.

--- ScrumPLE-Pretty/content/chat/chat.js
+++ ScrumPLE-Pretty/content/chat/chat.js
@@ -1,15 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
-//var io = require('socket.io')(https);
 var fs = require('fs');
 var https = require('https');
 var privateKey = fs.readFileSync('/etc/ssl/private/node-self.key');
 var certificate = fs.readFileSync('/etc/ssl/certs/node-self.crt');
 var credentials = {key: privateKey, cert: certificate};
 
-//var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 var io = require('socket.io')(httpsServer);
+
+var people = {};
 
 app.get('/', function(req, res){
     res.sendFile('/opt/git/scrumple/ScrumPLE-Pretty/content/chat/chat.html');
@@ -17,13 +17,20 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	console.log('user connected');
-	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
-	});
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
+	socket.on('join', function(name){
+		people[socket.userID] = name;
+		if(name != ''){
+			io.emit("chat message", name + ' has joined the lobby.');
+		}
+	
+		socket.on('chat message', function(msg){
+			console.log('message: ' + msg);
+			io.emit('chat message', name + ': ' + msg);
+		});
+		socket.on('disconnect', function(){
+			console.log('user disconnected');
+			io.emit('chat message', name + ' has disconnected');
+		});
 });
 
 httpsServer.listen(3000, function(){
