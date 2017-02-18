f9914488e01ba90608aa74c07a5945a34d94efad
kkorolyov <kirillk753@gmail.com>
2017-02-17 21:27:21 -0800

    Package rest.js things into 1 rest object

--- ScrumPLE-Pretty/content/lib/rest.js
+++ ScrumPLE-Pretty/content/lib/rest.js
@@ -1,49 +1,40 @@
-var credentials = null;
+var rest = {
+	credentials: null,
+	login: function(handle, password) {
+		this.credentials = btoa(handle + ":" + password);
+	},
+	getUrl: function(url) {
+		return "https://ec2-52-10-231-227.us-west-2.compute.amazonaws.com:8443/scrumple/rest/" + url;
+	},
+	/**
+	 * Sends a REST request.
+	 * 
+	 * @param {string} method request method
+	 * @param {string} url request URL starting after REST root
+	 * @param {Object} [content] request content
+	 * @param {function(string)} [responseHandler] invoked after server responds
+	 */
+	ajax: function(method, url, content, responseHandler) {
+		var xhttp = new XMLHttpRequest();
 
-var getUrl = function(url) {
-	return "https://ec2-52-10-231-227.us-west-2.compute.amazonaws.com:8443/scrumple/rest/" + url;
-}
-/**
- * Runs a request.
- * @param (string) method - Request method
- * @param (string) url - Request URL starting from the rest root
- * @param (string) content - Optional request content
- * @param (function(response)) responseHandler - Function invoked after server responds
- */
-function ajax(method, url, content, responseHandler) {	// Main REST invocation
-	var xhttp = new XMLHttpRequest();
-
-	if (responseHandler != null) {
-		xhttp.onreadystatechange = function() {
-			if (this.readyState === 4) {
-				var response = this.responseText;
-				try {
-					response = JSON.parse(response);
-				} catch (e) {
-					console.log(e.message);
+		if (responseHandler) {
+			xhttp.onreadystatechange = function () {
+				if (this.readyState === 4) {
+					var response = this.responseText;
+					try {
+						response = JSON.parse(response);
+					} catch (e) {
+						console.log(e.message);
+					}
+					responseHandler(response);
 				}
-				responseHandler(response);
 			}
 		}
-	}
-	xhttp.open(method, getUrl(url), true);
+		xhttp.open(method, this.getUrl(url), true);
 
-	if (credentials != null) {
-		xhttp.setRequestHeader("Authorization", "Basic " + credentials);
-	}
-	if (content != null) {
-		xhttp.setRequestHeader("Content-type", "application/json");
-	}
-	xhttp.send(content);
-}
+		if (this.credentials) xhttp.setRequestHeader("Authorization", "Basic " + this.credentials);
+		if (content) xhttp.setRequestHeader("Content-type", "application/json");
 
-/**
- * Changes the current value of the 'credentials' global.
- * Credentials are combined into a single string (HANDLE:PASSWORD) and Base64 encoded.
- * @param (string) handle - User handle
- * @param (string) password - User password
- */
-function login(handle, password) {
-	credentials = btoa(handle + ":" + password);
-	console.log("Logged in as: " + handle);
+		xhttp.send(content);
+	}
 }
