3ae297f1b2a8e0a4640265417de4f9a126baff4d
Ryan Truong <ryantruong1993@gmail.com>
2017-02-20 06:41:36 +0900

    Clean up chat js files.

--- ScrumPLE-Pretty/content/chat/js/joinChat.js
+++
@@ -1,7 +0,0 @@
-function joinChat(){
-			socket.emit('join', $('#n').val());
-			$('#n').val('');
-      document.getElementById('n').style.display='none';
-      document.getElementById('joinbtn').style.display='none';
-			return false;
-    }
--- ScrumPLE-Pretty/content/chat/js/sendMsg.js
+++
@@ -1,5 +0,0 @@
-    function sendMsg(){
-			socket.emit('chat message', $('#m').val());
-			$('#m').val('');
-      return false;
-    }
