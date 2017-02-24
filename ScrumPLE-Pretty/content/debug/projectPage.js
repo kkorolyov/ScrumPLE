03a2e4f69ff8fe3911ec2e6e025f061408dbb34e
Ryan Truong <ryantruong1993@gmail.com>
2017-02-25 06:42:06 +0900

    Update ProjectPage

--- ScrumPLE-Pretty/content/debug/projectPage.js
+++ ScrumPLE-Pretty/content/debug/projectPage.js
@@ -1,3 +1,22 @@
+function urlQuery(name, url) {
+    if(!url){
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if(!results[2]) return'';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function showProject(){
+    var projectId = urlQuery('projectId');
+    const url = "projects/" + projectId;
+    rest.ajax('GET', url, null, response => {
+        document.getElementById('project').innerHTML=response;
+    });
+}
 function logIn(){
     // if auth is successful, show project details and hide login form
     document.getElementById('login').style.display='none';
@@ -6,3 +25,4 @@ function logIn(){
 }
 
 document.getElementById("log").onclick=logIn;
+document.getElementById("log").onclick=showProject;
