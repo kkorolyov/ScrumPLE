6e468fb92638298204faf6fdbdd226167cc3592d
HOAMA <hoama2016@gmail.comhoama2016@gmail.com>
2017-03-12 05:54:13 -0700

    find js

--- ScrumPLE-Pretty/js/index.js
+++ ScrumPLE-Pretty/js/index.js
@@ -3,13 +3,25 @@ window.addEventListener('load', init);
 function init() {
 	document.getElementById('search_bar').addEventListener('input', event => {
 		var input = event.target.value;
-		var _str = '{"name":"'+input+'"}';
-		JSON.parse(_str);
-		rest.ajax('POST', '/rest/projects', _str, response => displayRaw(response));
+		if (!input) {
+			return;
+		}
+		//var _str = '{"name":"'+input+'"}';
+		//JSON.parse(_str);
+		rest.ajax('GET', 'projects?name='+input, null, response => displayRaw(response));
 	});
 }
 
 function displayRaw(response) {
 	var _str = (typeof response === 'string') ? response : JSON.stringify(response, null, 2);
-	document.getElementById('projectList').innerHTML = _str;
+	var innerHTML = '';
+	if (typeof response == 'object') {
+		innerHTML = '<ul>';
+		for (var i in response) {
+			if (response[i].visible)
+				innerHTML += '<li>' + response[i].name + '</li>';
+		}
+		innerHTML += '</ul>';
+	}
+	document.getElementById('projectList').innerHTML = innerHTML;
 }
