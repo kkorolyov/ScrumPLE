b6dbf54c84a6463c2c025722d93654d822fab7dd
michael's mac <enehnnamudi@gmail.com>
2017-03-13 19:19:17 -0700

    fixing the search bar

--- ScrumPLE-Pretty/js/index.js
+++ ScrumPLE-Pretty/js/index.js
@@ -1,27 +1,26 @@
+"use strict"
+
 window.addEventListener('load', init);
 
 function init() {
 	document.getElementById('search_bar').addEventListener('input', event => {
-		var input = event.target.value;
+		let input = event.target.value;
 		if (!input) {
 			return;
 		}
 		//var _str = '{"name":"'+input+'"}';
 		//JSON.parse(_str);
-		rest.ajax('GET', 'projects?name='+input, null, response => displayRaw(response));
-	});
-}
+		rest.ajax('GET','projects?name='+input, null, response => {
+			if (typeof response === 'object') {
+				for (let key in response ) {
+					let url = "/project/project.html?projectId=" + key
+					let name = response[key].name
+					let link = document.getElementById("project-list").appendChild("a")
+					link.href = url
+					link.textContent = name
+				}
 
-function displayRaw(response) {
-	var _str = (typeof response === 'string') ? response : JSON.stringify(response, null, 2);
-	var innerHTML = '';
-	if (typeof response == 'object') {
-		innerHTML = '<ul>';
-		for (var i in response) {
-			if (response[i].visible)
-				innerHTML += '<li style="cursor:pointer" onclick="window.open(\'view/login.html\',\'\',\'top=100,left=100,width=300,height=200\')">' + response[i].name + '</li>';
-		}
-		innerHTML += '</ul>';
-	}
-	document.getElementById('projectList').innerHTML = innerHTML;
+			}
+		};
+	});
 }
