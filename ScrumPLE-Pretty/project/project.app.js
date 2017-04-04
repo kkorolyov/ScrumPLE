9b71159e2e2cb2d1878391a6c2ed5fc68906f49c
kkorolyov <kirillk753@gmail.com>
2017-04-03 20:00:15 -0700

    Add ngRoute config

--- ScrumPLE-Pretty/project/project.app.js
+++ ScrumPLE-Pretty/project/project.app.js
@@ -1,10 +1,20 @@
 "use strict"
 
-angular
-	.module('project', ['login', 'users', 'tasks', 'stories', 'sprints', 'resources'])
-	.controller('project', ['$scope', '$location', 'resources', function ($scope, $location, resources) {
-		resources.project().id = $location.search().projectId
+const app = angular.module('project', ['login', 'users', 'tasks', 'stories', 'sprints', 'resources', 'ngRoute'])
 
-		$scope.title = () => { return (resources.auth()) ? "ScrumPLE - " + resources.project().name : "Log In To Project" }
-		$scope.resources = resources
-	}])
+app.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/users',	{
+			template: "<users></users>"
+		})
+		.when('/stories', {
+			template: "<stories></stories>"
+		})
+}])
+
+app.controller('project', ['$scope', '$location', 'resources', function ($scope, $location, resources) {
+	resources.project().id = $location.search().projectId
+
+	$scope.title = () => { return (resources.auth()) ? "ScrumPLE - " + resources.project().name : "Log In To Project" }
+	$scope.resources = resources
+}])
