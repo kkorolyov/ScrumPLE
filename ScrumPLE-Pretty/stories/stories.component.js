0bf525b41c121be0c693dc86ff95d0c926c49dbb
IamKenshin <ryantruong1993@gmail.com>
2017-04-06 23:06:26 -0700

    user stories page debugging.

--- ScrumPLE-Pretty/stories/stories.component.js
+++ ScrumPLE-Pretty/stories/stories.component.js
@@ -6,15 +6,35 @@ angular
 		templateUrl: "stories/stories.template.html",
 
 		bindings: {
-			stories: '<'
+			//stories: '<'
 		},
 		controller: ['resources' , function (resources) {
+			this.stories = []
 			const url = resources.projectUrl() + "/stories"
+			resources.get(url)
+				.then(stories => {
+					this.stories = stories
+					console.log(stories)
+				})
 			this.create = function() {
 				console.log("Create Button")
 				const newStory = {story: this.story, storyPoint: this.storyPoint}
 				// const url = resources.projectUrl() +"/stories"
 				resources.set(url, newStory)
+					.then(() => {
+						resources.get(url)
+							.then(stories => {
+								this.stories = stories
+								console.log("nested then")
+								console.log(this.stories)
+							})
+					})
+				/*resources.get(url)
+					.then(stories => {
+						this.stories = stories
+						console.log("Inside then:" + stories)
+					})*/
+				console.log("outside then: " + this.stories)
 			}
 
 			this.update = function(story) {
