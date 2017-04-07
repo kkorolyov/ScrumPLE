3e8c1da36faa0530de78d62cf2e5e08e34164876
IamKenshin <ryantruong1993@gmail.com>
2017-04-06 18:15:57 -0700

    imported JsonInclude.Include.

--- ScrumPLE-Core/src/main/java/org/scrumple/scrumplecore/scrum/UserStory.java
+++ ScrumPLE-Core/src/main/java/org/scrumple/scrumplecore/scrum/UserStory.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @JsonInclude(Include.NON_NULL)
 public class UserStory {
