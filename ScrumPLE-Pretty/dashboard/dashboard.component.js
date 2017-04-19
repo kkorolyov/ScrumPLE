817707f4246bc32529c07606428b7527cdb7864a
michael's mac <enehnnamudi@gmail.com>
2017-04-19 16:05:52 -0700

    dashboard component still in progres

--- ScrumPLE-Pretty/dashboard/dashboard.component.js
+++ ScrumPLE-Pretty/dashboard/dashboard.component.js
@@ -10,6 +10,47 @@ angular
 		},
 
         controller:['$uibModal', 'resources', function ($uibModal, resources) {
+
             this.names = [ "InProgress", "Not Started","Completed"];
+
+            const meetingurl = resources.projectUrl() + "/meetings"
+            const taskurl = resources.projectUrl() + "/stories" + "/" + story.id + "/tasks"
+            const sprinturl = resources.projectUrl() + "/sprints"
+
+            const meetings = []
+            const tasks = []
+            const sprint = []
+
+            this.showmeeting = function (meetingurl, meetings) {
+                const currentDate = new Date ();
+                resources.get(meetingurl)
+                    .then(meeting => {
+                        for(let i =0; i < meeting.length; i++){
+                            if(meeting[i].start && meeting[i].end === currentDate){
+                                meetings.push(meeting[i])
+                            }
+                        }
+                    
+                    })
+                    return meetings
+            }
+
+            this.showsprint = function (sprinturl, sprints) {
+                const currentDate = new Date ();
+                resources.get(sprinturl)
+                    .then(sprint => {
+                        for(let i =0; i < sprint.length; i++){
+                            // wnated to check for the whole week from start to finish so it keeps on shoinw the same sprint number
+                            if(sprint[i].start === currentDate){
+                                sprints.sprintNmber.push(sprint[i])
+                            }
+                        }
+                    })
+                    return sprints
+            }
+
+            this.showtask = function (taskurl, stories){
+                
+            }
         }]
     })
