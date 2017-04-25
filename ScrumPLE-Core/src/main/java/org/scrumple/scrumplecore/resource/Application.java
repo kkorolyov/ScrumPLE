5b8bf3ef54acd1a196d6b9c71e969753aa0fa4cd
kkorolyov <kirillk753@gmail.com>
2017-04-25 13:32:39 -0700

    Change MySQL connector init

--- ScrumPLE-Core/src/main/java/org/scrumple/scrumplecore/resource/Application.java
+++ ScrumPLE-Core/src/main/java/org/scrumple/scrumplecore/resource/Application.java
@@ -5,15 +5,10 @@ import java.net.URISyntaxException;
 import java.util.Enumeration;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 import javax.ws.rs.core.Context;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.scrumple.scrumplecore.assets.Assets;
-
-import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
 
 import dev.kkorolyov.simpleprops.Properties;
 
@@ -32,22 +27,5 @@ public class Application extends ResourceConfig {
 			props.put(key, value);
 		}
 		return props;
-	}
-
-	@WebListener
-	public static class ThreadCleanupListener implements ServletContextListener {
-		@Override
-		public void contextDestroyed(ServletContextEvent arg0) {
-			try {
-				AbandonedConnectionCleanupThread.shutdown();	// MySQL connection lingers for some reason
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override
-		public void contextInitialized(ServletContextEvent arg0) {
-			// Ignore
-		}
 	}
 }
